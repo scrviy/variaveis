ca68e8b6b7e8b3a12a9a4e0d6e8443578db6a661
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-14 15:33:29 -0300

    doc about variables

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-let nome; // Declarou a variável
-nome = "João"; // Inicializando a variável
-console.log(nome);
+// Não podemos criar variáveis com palavras reservadas da linguagem
+// Variáveis precisam ter nomes significativos
+// Não começar o nome de uma variável com um número
+// Não podem conter espacos, traços ou caracteres especiais
+// Variaveis no javascript utilizam camelCase
+// Javascript é case-sensitive
+// Não é possível redeclarar variáveis com let
+// Não utilizar var, preferir por let ao criar variáveis
 
-nome = "Junior";
-console.log(nome);
+let nomeCliente; // Declarou a variável
+nomeCliente = "João Marcos"; // Inicializando a variável
+console.log(nomeCliente);
+
+nomeCliente = "Carlos Junior"; // Redeclarando a variável `nome`
+console.log(nomeCliente);
+
+
+let nomeCompleto = "Lucas Silva";
+let nomecompleto = "Yuri Ramos";
+
+console.log(nomeCompleto);
+console.log(nomecompleto);
